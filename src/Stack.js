7c28f30735128a49ccf79f8841029cc12a58fa6c
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-07 15:30:06 -0400

    add push() method

--- src/Stack.js
+++ src/Stack.js
@@ -10,5 +10,12 @@ class Node {
     constructor() {
       this.top = null;
     }
+    push(value) {
+        // Create a new node,
+        // add data to the new node, and
+        // have the pointer point to the top
+        this.top = new Node(value, this.top);
+        return this;
+      }
     
   }

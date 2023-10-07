75e841d42896cc8a679df6a1bf244010314b7221
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-07 15:30:46 -0400

    create pop() method

--- src/Stack.js
+++ src/Stack.js
@@ -17,5 +17,12 @@ class Node {
         this.top = new Node(value, this.top);
         return this;
       }
-    
+      pop() {
+        /* In order to remove the top of the stack, you have to point
+            the pointer to the next item. That next item becomes the
+            top of the stack. */
+        const popped = this.top;
+        this.top = popped.next;
+        return popped.value;
+    }
   }
